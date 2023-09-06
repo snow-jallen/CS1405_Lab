e3bbd5d2c09097bb87905e84f2d0cf9211a36003
J Allen <42281341+snow-jallen@users.noreply.github.com>
2023-09-06 07:57:24 -0600

    Update Program.cs
    use 'top level statements' template

--- Program.cs
+++ Program.cs
@@ -1,12 +1,2 @@
-﻿using System;
-
-namespace CS14X5_Lab
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿// CS 14x5 Lab Template
+Console.WriteLine("Hello World!");
